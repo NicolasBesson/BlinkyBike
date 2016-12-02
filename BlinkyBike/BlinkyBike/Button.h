cb565bfc2f4a4849cbb2c9df35234e2f50ee4bc4
Nicolas Besson <billounet@gmail.com>
2016-12-02 22:31:46 +0100

    Updating Headers copyright for 2017
    Signed-off-by: Nicolas Besson <billounet@gmail.com>

--- BlinkyBike/BlinkyBike/Button.h
+++ BlinkyBike/BlinkyBike/Button.h
@@ -1,6 +1,6 @@
 /////////////////////////////////   License  //////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-// Copyright 2015 Nicolas BESSON - BeeAtHome
+// Copyright 2015 - 2017 Nicolas BESSON - BeeAtHome
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
